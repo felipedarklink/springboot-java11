12644e395bf4517b5726d2c63c610c1567299dcb
Felipe Silva <felipeosilva@hotmail.com>
2021-04-30 20:15:00 -0300

    Project created

---
+++ src/test/java/com/felipeosilva/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipeosilva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
